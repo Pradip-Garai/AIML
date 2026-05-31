d109ecde139c033da235e7b58942a827354dfca9
Pradip-Garai <pradipgarai173@gmail.com>
2026-05-31 17:33:08 +0530

    basic atatypes of python

---
+++ Python/datatypes.py
@@ -0,0 +1,47 @@
+# Integer
+a = 8
+print(a)
+type(a)
+
+# Floating Point
+b = 2.36
+print(b)
+type(b)
+
+# Complex Numbers
+c = 1 + 3j
+print(c)
+type(c)
+
+# int to float conversion
+x = 10
+print(x)
+type(x)
+
+y = float(x)
+print(y)
+type(y)
+
+# float to int conversion
+x = 50.10
+print(x)
+type(x)
+
+y = int(x)
+print(y)
+type(y)
+
+# Boolean
+a = True
+type(a)
+
+b = False
+type(b)
+
+a = 7 < 3
+print(a)
+
+# String
+name = "Pradip Garai"
+print(name)
+type(name)
